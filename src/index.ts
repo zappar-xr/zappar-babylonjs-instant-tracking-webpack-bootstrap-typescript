2c49547b704807a79133ca546c431d8fa2880c9b
Zappar <hello@zappar.com>
2021-01-26 13:59:26 +0000

    Minimal webpack project for Zappar BabylonJS instant world tracking (TypeScript)

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as BABYLON from 'babylonjs';
+import * as ZapparBabylon from '@zappar/zappar-babylonjs';
+
+import "./style.css";
+
+
+// Setup BabylonJS in the usual way
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+const engine = new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true
+});
+
+
+export const scene = new BABYLON.Scene(engine);
+const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+// Setup a Zappar camera instead of one of Babylon's cameras
+export const camera = new ZapparBabylon.Camera(scene);
+
+// Request the necessary permission from the user
+ZapparBabylon.permissionRequestUI().then((granted) => {
+    if (granted) camera.start();
+    else ZapparBabylon.permissionDeniedUI();
+});
+
+const instantWorldTracker = new ZapparBabylon.InstantWorldTracker();
+const trackerTransformNode = new ZapparBabylon.InstantWorldAnchorGroup(camera, instantWorldTracker);
+
+// Add some content to the instant tracker
+const box = BABYLON.Mesh.CreateBox('box', 1, scene, false, BABYLON.Mesh.DOUBLESIDE)
+box.parent = trackerTransformNode;
+
+let hasPlaced = false;
+
+const button = document.getElementById('zappar-placement-ui') || document.createElement('div');
+button.addEventListener('click', () => {
+    hasPlaced = true;
+});
+
+window.addEventListener('resize', () => {
+    engine.resize();
+});
+
+// Set up our render loop
+engine.runRenderLoop(() => {
+    camera.updateFrame();
+    if (!hasPlaced) {
+        instantWorldTracker.setAnchorPoseFromCameraOffset(0, 0, -5);
+    }
+    scene.render();
+});
